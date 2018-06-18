d9d475697cccf6788cacd5a39e0b19786a9411f6
Patrick Grogan <pgrogan@vigilantglobal.com>
2018-06-18 13:37:12 -0400

    Added default pylint and flake8 config files.

--- configure.sh
+++ configure.sh
@@ -3,4 +3,6 @@ ln -s -f $PWD/.vimrc ~
 ln -s -f $PWD/.zshrc ~
 ln -s -f $PWD/.oh-my-zsh ~
 ln -s -f $PWD/.tmux/.tmux.conf ~
+ln -s -f $PWD/.flake8 ~
+ln -s -f $PWD/.pylintrc ~
 vim +PluginInstall +qall
