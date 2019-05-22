704dccc56b548d4db007930f454c40a45966eca1
Patrick Grogan <pgrogan@lmp-mtpgrogan02.us.drwholdings.com>
2019-05-21 20:31:58 -0400

    Adding simlink to pure_power script file in home .zshrc

--- configure.sh
+++ configure.sh
@@ -3,6 +3,7 @@ ln -s -f $PWD/init.vim ~/.config/nvim
 ln -s -f $PWD/vimrc/ ~/.config/nvim/vimrc
 ln -s -f $PWD/.zshrc ~
 ln -s -f $PWD/.oh-my-zsh ~
+ln -s -f $PWD/pure_power ~/.pure_power
 ln -s -f $PWD/.tmux/.tmux.conf ~
 ln -s -f $PWD/.flake8 ~
 ln -s -f $PWD/.pylintrc ~
