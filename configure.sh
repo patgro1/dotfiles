f004379e9a476eae9d28efd141ed6ec99214140a
Patrick Grogan <pogrogan@gmail.com>
2019-12-01 21:12:12 -0500

    Added folders to better split the dot files and added intitial emacs config.

--- configure.sh
+++ configure.sh
@@ -1,11 +1,10 @@
 # Configurating the shell and themes
-ln -s -f $PWD/.zshrc ~
-ln -s -f $PWD/.oh-my-zsh ~
+ln -s -f $PWD/zsh/.zshrc ~
+ln -s -f $PWD/zsh/.oh-my-zsh ~
 # zsh syntax highlighting
-if [ ! -d zsh-syntax-highlighting ];then
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+if [ ! -d zsh/.oh-my-zsh/plugins/zsh-syntax-highlighting ];then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ./zsh/.oh-my-zsh/plugins/zsh-syntax-highlighting
 fi
-ln -s -f $PWD/zsh-syntax-highlighting $PWD/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 if [ ! -d ./dracula ]; then
     mkdir -p dracula
 fi
@@ -15,14 +14,20 @@ if [ ! -d zsh ]; then
 fi
 ln -s -f $PWD/zsh/dracula.zsh-theme ~/.oh-my-zsh/themes/dracula.zsh-theme
 cd ../
-mkdir -p ~/.config/nvim/
-ln -s -f $PWD/init.vim ~/.config/nvim
-ln -s -f $PWD/vimrc/ ~/.config/nvim/vimrc
-ln -s -f $PWD/.tmux/.tmux.conf ~
+if [ ! -d ~/.config/nvim ]; then
+    ln -s -f $PWD/vim ~/.config/nvim
+fi
+ln -s -f $PWD/tmux/.tmux/.tmux.conf ~
 ln -s -f $PWD/.gitconfig ~
-ln -s -f $PWD/.flake8 ~
-ln -s -f $PWD/.pylintrc ~
+ln -s -f $PWD/python/.flake8 ~
+ln -s -f $PWD/python/.pylintrc ~
 ln -s -f $PWD/.ctags ~
-nvim +PlugInstall +qall
-ln -s -f $PWD/tmuxinator.zsh ~/.tmuxinator.zsh
-ln -s -f $PWD/tabular_extra.vim ~/.local/share/nvim/plugged/tabular/after/plugin/tabular_extra.vim
+#nvim +PlugInstall +qall
+ln -s -f $PWD/tmux/tmuxinator.zsh ~/.tmuxinator.zsh
+
+# Emacs config
+if [ ! -d ~/.emacs.d ]; then
+    mkdir ~/.emacs.d
+fi
+ln -sf $PWD/emacs/init.el ~/.emacs.d
+ln -sf $PWD/emacs/conf.org ~/.emacs.d
---
+++ emacs/init.el
@@ -0,0 +1,23 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
+(setq package-enable-at-startup nil)
+(unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (package-install 'use-package))
+(require 'org)
+(org-babel-load-file
+ (expand-file-name "conf.org"
+                   user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(use-package linum-relative evil dracula-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
