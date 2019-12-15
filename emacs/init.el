5e5c3a5056ee47fa3c960b1dd6327c29f974041c
Patrick Grogan <pogrogan@gmail.com>
2019-12-15 15:36:51 -0500

    Added ivy and counsel mode to my config

--- emacs/init.el
+++ emacs/init.el
@@ -16,8 +16,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (multi-term use-package linum-relative evil dracula-theme))))
+   '(counsel ivy multi-term use-package linum-relative evil dracula-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
