e907d1c6b09cadad9f7a185817cc5abfa1c73388
Patrick Grogan <pgrogan@drw.com>
2020-12-10 09:46:16 -0500

    Final commit in doom.d before single repo

---
+++ .doom.d/configuration.el
@@ -0,0 +1,4 @@
+(after! org
+  (set-face-attribute 'org-table nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-block nil :inherit 'fixed-pitch))
