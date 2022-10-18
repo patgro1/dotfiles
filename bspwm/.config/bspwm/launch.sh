51fa2ad14a60e56b1014ab37b1a08a2d3a00cf46
po.grogan@gmail.com <po.grogan@gmail.com>
2022-10-18 12:39:51 -0400

    Reverted changes for the toggling polybar

--- bspwm/.config/bspwm/launch.sh
+++ bspwm/.config/bspwm/launch.sh
@@ -6,13 +6,6 @@ function run() {
     pgrep $1 >> /dev/null || $1 &
 }
 
-if type mons; then
-    if [ $(xrandr -q | grep " connected" | wc -l) == 2 ]; then
-        mons -e right --primary DP-3-1
-    fi
-fi
-
-
 run sxhkd
 killall picom
 picom -b
@@ -21,8 +14,4 @@ run nm-applet
 run blueman-applet
 run flameshot
 
-pgrep spotify >> /dev/null || flatpak run com.spotify.Client&
-
-if type mons; then
-    pgrep mons >> /dev/null || mons -ax $XDG_CONFIG_HOME/bspwm/dock.sh
-fi
+pgrep mons >> /dev/null || mons -ax $XDG_CONFIG_HOME/bspwm/dock.sh
--- bspwm/.config/bspwm/polybar/launch.sh
+++ bspwm/.config/bspwm/polybar/launch.sh
@@ -3,14 +3,12 @@
 killall polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
  
-for m in $(polybar -m | cut -d":" -f1); do
-    MONITOR=$m polybar -c $XDG_CONFIG_HOME/bspwm/polybar/config.ini --reload workspaces&
-done
-polybar -c $XDG_CONFIG_HOME/bspwm/polybar/config.ini --reload media&
-polybar -c $XDG_CONFIG_HOME/bspwm/polybar/config.ini --reload system_status&
-sleep 1
-xdo raise -N Polybar
-polybar-msg cmd hide
-
+if type "xrandr"; then
+    for m in $(polybar -m | cut -d":" -f1); do
+        MONITOR=$m polybar -c $XDG_CONFIG_HOME/bspwm/polybar/config.ini --reload top &
+    done
+else
+    polybar --reload top &
+fi
 
 
