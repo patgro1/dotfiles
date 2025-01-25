24ca7fe91f7d9f46da0fa029cb0cd06cfe545afa
Patrick Grogan <patrick.grogan@enyx.fr>
2025-01-24 23:24:00 -0500

    Custom fish prompt without tide

--- config/dot_config/fish/functions/fish_prompt.fish
+++ config/dot_config/fish/functions/fish_prompt.fish
@@ -1,10 +1,8 @@
 function fish_prompt
     set -l last_status $transient_status
     set -l prompt_color (set_color green)
-    if test $last_status -ne 0
-        set prompt_color (set_color red)
-    end
-    echo (set_color normal)$PWD
+    
+    echo (set_color cyan)$PWD (fish_git_prompt)
     echo $prompt_color"> "
 end
 
@@ -38,3 +36,34 @@ function fish_mode_prompt
     end
     set_color normal
 end
+
+function _get_git_prompt
+    set --local git_branch (_git_branch)
+    echo (set_color grey)"($git_branch)"
+end
+
+function _git_branch
+    command git symbolic-ref --short HEAD 2>/dev/null;
+        or command git rev-parse --short HEAD 2>/dev/null
+end
+
+set -g __fish_git_prompt_show_informative_status 1
+set -g __fish_git_prompt_showuntrackedfiles 0
+
+set -g __fish_git_prompt_color_branch magenta
+set -g __fish_git_prompt_showupstream "informative"
+set -g __fish_git_prompt_char_upstream_ahead "↑"
+set -g __fish_git_prompt_char_upstream_behind "↓"
+set -g __fish_git_prompt_char_upstream_prefix ""
+
+set -g __fish_git_prompt_char_stagedstate "●"
+set -g __fish_git_prompt_char_dirtystate "✚"
+set -g __fish_git_prompt_char_untrackedfiles "…"
+set -g __fish_git_prompt_char_conflictedstate "✖"
+set -g __fish_git_prompt_char_cleanstate "✔"
+
+set -g __fish_git_prompt_color_dirtystate blue
+set -g __fish_git_prompt_color_stagedstate yellow
+set -g __fish_git_prompt_color_invalidstate red
+set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+set -g __fish_git_prompt_color_cleanstate green
